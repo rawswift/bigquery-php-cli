10f7016cd2d48903f4b07cb56165477a9e27afc8
Ryan Yonzon <rawswift@gmail.com>
2014-08-17 14:48:13 +0800

    Add list dataset (sample) script.

---
+++ cli-list-dataset.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * List dataset(s)
+ * Written by Ryan Yonzon <hello@ryanyonzon.com>
+ */
+
+set_include_path("vendor/google/apiclient/src/" . PATH_SEPARATOR . get_include_path());
+
+require_once 'Google/Client.php';
+require_once 'Google/Service/Bigquery.php';
+
+// configuration stuff
+$config = require_once 'config/Credentials.php';
+
+$project_id = 'PROJECT-ID-HERE';
+
+// create Google Client
+$client = new Google_Client();
+$client->setApplicationName("BigQuery CLI");
+
+$key = file_get_contents('key/' . $config->key_filename);
+
+$client->setAssertionCredentials(new Google_Auth_AssertionCredentials(
+    $config->service_account_name,
+    array(
+        'https://www.googleapis.com/auth/bigquery'
+        ),
+        $key
+    )
+);
+$client->setClientId($config->client_id);
+
+// create BigQuery service
+$service = new Google_Service_Bigquery($client);
+
+// list dataset(s)
+$options = array();
+$response = $service->datasets->listDatasets($project_id, $options);
+
+print_r($response);
